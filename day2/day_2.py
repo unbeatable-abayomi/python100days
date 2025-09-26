d89ae7b577b09e55f4c88f005b563710f361782e
unbeatable-abayomi <igwubor@outlook.com>
2025-09-26 15:53:30 +0100

    Added PEMDASLR note

--- day2/day_2.py
+++ day2/day_2.py
@@ -5,6 +5,20 @@ import sys
 print(sys.version.split()[0])
 
 age = 20
+print("Hello"[0])
 
 print(f"I am {age} years old."[1])
 print(type(123_456_789))
+
+number = input("Type a two digit number:\n")
+print(int(number[0]) + int(number[1]))
+
+# PEMDASLR
+# Parentheses ()
+# Exponents **
+# Multiplication * Division /
+# Addition + Subtraction -
+
+
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
