a40ad4b1102bae3baf952cbe9d82366f33400c89
unbeatable-abayomi <igwubor@outlook.com>
2025-09-28 05:28:10 +0100

    Control Flow Modification

--- day3/day_3.py
+++ day3/day_3.py
@@ -1,1 +1,8 @@
 # Control flow and logical operators
+
+age_max = int(input("What is your age? :"))
+
+if age_max >= 18:
+    print("You are old enough to drink alcohol")
+else:
+    print("You are not old enough to drink alcohol")
