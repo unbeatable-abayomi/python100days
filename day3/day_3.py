aee1d53d46b79c9e2aaa42878ed2ee1b60fcefd9
unbeatable-abayomi <igwubor@outlook.com>
2025-09-28 05:42:06 +0100

    added even and odd number check

--- day3/day_3.py
+++ day3/day_3.py
@@ -6,3 +6,10 @@ if age_max >= 18:
     print("You are old enough to drink alcohol")
 else:
     print("You are not old enough to drink alcohol")
+
+num = int(input("Enter your number : "))
+
+if num % 2 == 0:
+  print(f"{num} is a an even number")
+else:
+ print(f"{num} is a an odd number")
