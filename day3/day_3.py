c18b678a045c36f9400652ba5851c9a5c168ee88
unbeatable-abayomi <igwubor@outlook.com>
2025-09-29 05:48:42 +0100

    day3 WIP

--- day3/day_3.py
+++ day3/day_3.py
@@ -1,15 +1,27 @@
 # Control flow and logical operators
 
-age_max = int(input("What is your age? :"))
+# age_max = int(input("What is your age? :"))
 
-if age_max >= 18:
-    print("You are old enough to drink alcohol")
+# if age_max >= 18:
+#     print("You are old enough to drink alcohol")
+# else:
+#     print("You are not old enough to drink alcohol")
+
+# num = int(input("Enter your number : "))
+
+# if num % 2 == 0:
+#   print(f"{num} is a an even number")
+# else:
+#  print(f"{num} is a an odd number")
+
+height = int(input("Enter your height: " ))
+
+age = int(input("Enter your age:"))
+
+if height >= 120:
+   if age >= 19:
+      print(f"You will pay normal price")
+   else:
+      print(f"You will pay have a discount price")
 else:
-    print("You are not old enough to drink alcohol")
-
-num = int(input("Enter your number : "))
-
-if num % 2 == 0:
-  print(f"{num} is a an even number")
-else:
- print(f"{num} is a an odd number")
+ print("Not eligible to ride")
