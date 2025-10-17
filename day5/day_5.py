7cccd4283e5d2550f4010ecf393f4abf97f05b2b
unbeatable-abayomi <igwubor@outlook.com>
2025-10-17 20:04:03 +0100

    Added alternative to Sum and Len function

--- day5/day_5.py
+++ day5/day_5.py
@@ -15,6 +15,16 @@ for n in range(0, len(splitted_mynumber)):
     
 print(splitted_mynumber)
 all_splitted_mynumber = 0
+number_of_elements = 0
+sum_of_elements = 0
+print(number_of_elements, "first")
+for x in splitted_mynumber:
+    print(f"{x} is in the array")
+    number_of_elements += 1
+    sum_of_elements += x
+print(number_of_elements, "second")
+print(round(sum_of_elements/number_of_elements), "second method")
+
 for n in range(0, len(splitted_mynumber)):
     all_splitted_mynumber += int(splitted_mynumber[n])
     
