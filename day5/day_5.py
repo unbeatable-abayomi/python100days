9e077f4e2879b4016007defdff5d87088eaf403e
unbeatable-abayomi <igwubor@outlook.com>
2025-10-17 21:59:15 +0100

    Added logic for highest number

--- day5/day_5.py
+++ day5/day_5.py
@@ -13,15 +13,28 @@ print(splitted_mynumber)
 for n in range(0, len(splitted_mynumber)):
     splitted_mynumber[n] = int(splitted_mynumber[n])
     
-print(splitted_mynumber)
+print(splitted_mynumber , "after converting to int", type(splitted_mynumber[0]))
+
 all_splitted_mynumber = 0
 number_of_elements = 0
 sum_of_elements = 0
+container_var = 0
+highest_value = splitted_mynumber[0]
+
 print(number_of_elements, "first")
+print(highest_value, "firstdd")
+
 for x in splitted_mynumber:
     print(f"{x} is in the array")
     number_of_elements += 1
     sum_of_elements += x
+    # container_var = x
+    if x > highest_value:
+      highest_value = x
+
+
+
+print(f"{highest_value} is the highest")
 print(number_of_elements, "second")
 print(round(sum_of_elements/number_of_elements), "second method")
 
