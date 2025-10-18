cc34ea6db9f20d8af576ae50e3c4f7c9edfdbe14
unbeatable-abayomi <igwubor@outlook.com>
2025-10-18 10:37:44 +0100

    Added Random Password Generator

--- day5/day_5.py
+++ day5/day_5.py
@@ -1,6 +1,7 @@
 # /bin/python3
 
 # Loops
+import random
 
 mynumber = input("give me numbers you want in an array ")
 
@@ -71,3 +72,30 @@ for num in range (1, 101):
     else:
       print(num) 
 
+
+
+print("My password Gen")
+
+allalphas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+allspecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "\\", "|", ";", ":", "'", "\"", ",", "<", ".", ">", "/", "?"]
+
+allnumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+# passwordLenght = int(input("How many character do you want in your password"))
+countOfLetter = int(input("How many letters do you want in your password : "))
+countOfSpecial = int(input("How many Special character do you want in your password : "))
+countOfnumbers = int(input("How many Numbers do you want in your password : "))
+
+passwordGenerated = ''
+for pl in range(1, countOfLetter+1):
+    selectedLetter = random.randint(0,len(allalphas)-1)
+    passwordGenerated += allalphas[selectedLetter]
+for pl in range(1, countOfSpecial+1):
+    selectedSpecial = random.randint(0,len(allspecial)-1)
+    passwordGenerated += allspecial[selectedSpecial]
+for pl in range(1, countOfnumbers+1):
+    selectedNumber = random.randint(0,len(allnumber)-1)
+    passwordGenerated += allnumber[selectedNumber]
+    
+print(f" Here is your password {passwordGenerated}")
