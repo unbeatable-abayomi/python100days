9c6666564ba91a222e08ea30bef855e5824c7640
unbeatable-abayomi <igwubor@outlook.com>
2026-02-12 13:02:43 +0100

    Hurdle 4

--- day6/day_6.py
+++ day6/day_6.py
@@ -130,3 +130,30 @@ while at_goal() != True:
 # Hurdle = 3 Yet Another way
 while not at_goal():
       jump()      
+
+
+# Hurdle 4
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+
+
+def jump():
+    turn_left()
+    while wall_on_right():
+      move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+while at_goal() != True :
+      if wall_in_front():
+         jump()
+      else:
+        move()
