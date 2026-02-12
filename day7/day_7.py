55c1dc1073d7e6476f87bfe4ef3fbdfbe78a1939
unbeatable-abayomi <igwubor@outlook.com>
2026-02-13 00:11:52 +0100

    HangMan WIP

--- day7/day_7.py
+++ day7/day_7.py
@@ -10,17 +10,36 @@ word_list = ["ardvark","baboon","camel"]
 chosen_word = word_list[r]
 guess = input("Guess a letter: ").lower()
 print(chosen_word, guess)
+outputs = []
+for n in chosen_word:
+    outputs.append("_")
 
-for letter in chosen_word:
-    if letter == guess:
-        print(f"Yes {letter} {guess}")
-    else:
-        print (f"No {letter} {guess}")
+print(outputs)
 
-if guess in chosen_word:
-    print(f"Yes {guess} is in the word {chosen_word}")
 
-else:
-    print(f"No {guess} is not the word {chosen_word}")
+for p in range(len(chosen_word)):
+    if chosen_word[p] == guess:
+      
+        outputs[p]=guess
+    # else: 
+      
+    #     outputs[p]= '_'
+# for index, letter in enumerate(chosen_word):
+#     if letter == guess:
+#         print(f"Yes {letter} {guess}")
+#         outputs[index]=guess
+#     else:
+#         print (f"No {letter} {guess}")
+#         outputs[index]= '_'
+
+print(outputs)
+# if guess in chosen_word:
+#     print(f"Yes {guess} is in the word {chosen_word}")
+
+# else:
+#     print(f"No {guess} is not the word {chosen_word}")
+
+
+
 
 
