6b28ec959900a881e2ec29a645855e54e67284fd
unbeatable-abayomi <igwubor@outlook.com>
2026-02-28 03:15:36 +0100

    Added Minimal Function with Parameter

--- day8/day_8.py
+++ day8/day_8.py
@@ -6,3 +6,11 @@ def greet():
     print("Going Smooth")
     
 greet()
+
+def greet_with_name(name):
+    print(f'Hello {name}')
+    print(f'Hope everthing is fine {name}')
+    print(f"All going smooth {name}")
+
+name = 'abayomi'
+greet_with_name(name)
