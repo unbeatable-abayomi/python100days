73ad28c23c3ff5e2c0e57ae6e0b32ab15381f067
unbeatable-abayomi <igwubor@outlook.com>
2026-03-15 02:46:37 +0100

    Added Prime number checker

--- day8/day_8.py
+++ day8/day_8.py
@@ -48,3 +48,18 @@ restules = print_cal(width,height,coverage)
 
 
 print(math.ceil(restules))
+
+check_number = int(input("Input the number to check: "))
+
+def prime_checker(number=check_number):
+     is_prime = True
+     for i in range(2,number):
+          if number % i == 0:
+            is_prime = False
+     if is_prime :
+          print(f"{number} is a prime number")
+     else:
+           print(f"{number} is not a prime number")
+
+
+prime_checker(check_number)
