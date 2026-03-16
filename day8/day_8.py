00076d502fb4083ae16d9e85569e332afbdacd30
unbeatable-abayomi <igwubor@outlook.com>
2026-03-16 14:51:21 +0100

    Added Cipher Function

--- day8/day_8.py
+++ day8/day_8.py
@@ -63,3 +63,30 @@ def prime_checker(number=check_number):
 
 
 prime_checker(check_number)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+# direction = input("Type 'encode' or 'decode' to do either").lower()
+
+text = input("Type the text that you want to either encode or decode: ").lower()
+
+shift_n = int(input("input shift: "))
+
+def encrpty (plain_text, shift_amount):
+     cipher_text = ''
+     for letter in plain_text:
+        position=alphabet.index(letter)
+        shift_position = position + shift_amount
+        print(f'shift post {shift_position} and lenfgt {len(alphabet)}')
+        if shift_position >= len(alphabet):
+            new_shift_position = shift_position - len(alphabet)
+            new_letter = alphabet[new_shift_position]
+        else:
+          new_letter = alphabet[shift_position]
+        cipher_text += new_letter
+     print(f'The is the encrptyed text {cipher_text}')
+
+
+
+encrpty(plain_text=text,shift_amount=shift_n)
