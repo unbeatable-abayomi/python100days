71b835300d6391f767e62b8444fb0e2bc149808e
unbeatable-abayomi <igwubor@outlook.com>
2026-07-14 09:08:32 +0100

    Updating Dictionary Function

--- day9/day_9.py
+++ day9/day_9.py
@@ -2,6 +2,106 @@ programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected.",
     "Function": "A piece of code that you can easily call over and over again.",
     "Loop": "The action of doing something over and over again.",
+    123:12345
 }
 
-print(programming_dictionary["Bug"])
+
+#Retrieving items from dictionary
+print(programming_dictionary["Bug"],programming_dictionary[123])
+
+#Adding new items to dictionary
+programming_dictionary["error"] = "Syntax Error"
+
+
+#Create an empty dictionary
+empty_dictionary = {}
+print(programming_dictionary["error"])
+#Editing items in dictionary
+programming_dictionary["error"] = "Runtime Error"
+print(programming_dictionary["error"])
+print(programming_dictionary)
+
+#Wipe an existing  dictionary
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+#Loop through a dictionary
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
+
+
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+student_results = {}
+for score in student_scores:
+   if student_scores[score] > 90 or student_scores[score] == 100:
+       student_results[score]="Outstanding"
+   elif student_scores[score] > 80 and student_scores[score] < 91:
+       student_results[score]="Exceeds Expectations"
+   elif student_scores[score] > 70 and student_scores[score] < 81:
+    student_results[score]="Acceptable"
+   elif student_scores[score] < 70:
+       student_results[score]="Fail"
+
+print(student_results)
+student_grades = {}
+for student in student_scores:
+   score = student_scores[student]
+   if score > 90:
+      student_grades[student] = "Outstanding"
+   elif score > 80:
+      student_grades[student] = "Exceeds Expectations"
+   elif score > 70: 
+       student_grades[student] = "Acceptable"
+   else:
+      student_grades[student] = "Fail"
+
+print(student_grades)
+
+
+#Nesting dictionary inside of a dictionary
+travel_log = {
+   "France" : {"cities_visited":["Paris","Lille","Dijion"],"total_visits":12},
+   "Germany" : {"cities_visited": ["Berlin","Hamburg","Stuggart"],"total_visits":5},
+  # "Nigeria": {"LAGOS":200,"ABUJA":300,"JOS":400}
+
+}
+
+
+#Nesting dictionary inside of a list
+travel_log2 = [
+   {
+      "country":"France",
+      "cities_visited":["Paris","Lille","Dijion"],
+      "total_visits":12
+   },
+   {
+      "country":"Germany",
+      "cities_visited": ["Berlin","Hamburg","Stuggart"],
+      "total_visits":5
+   },
+]
+
+def add_new_country(country2,lists_of_cities,visits):
+#    travel_log2.append({'country':country2,"cities_visited":lists_of_cities,"total_visits":visits})
+   # or
+   new_country = {}
+   new_country["country"] = country2
+   new_country["cities_visited"] = lists_of_cities
+   new_country["total_visits"] = visits
+   travel_log2.append(new_country)
+
+add_new_country("Brazil",['Sao Paulo','Rio de Janerio'],5)
+
+print(f"I've been to {travel_log2[2]['country']} {travel_log2[2]['total_visits']} times...")
+
+print(f"My favorite city was {travel_log2[2]['cities_visited'][0]}")
+   
+
