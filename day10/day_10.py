6ce52808ce855050d4539b52cf3f6b593f3021fa
unbeatable-abayomi <igwubor@outlook.com>
2026-07-21 12:09:38 +0100

    Wrote leap year Function

--- day10/day_10.py
+++ day10/day_10.py
@@ -10,3 +10,18 @@ name = format_name(f,l)
 
 
 print(name)
+def is_leap (year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("Leap year")
+            else:
+                print("Not leap year")
+        else:
+            print("Leap year")
+    else:
+        print("Not leap year")
+
+# TODO: Add more code here 👇
+def days_in_month():
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
